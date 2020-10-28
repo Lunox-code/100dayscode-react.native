a8b6003319fd9fb840dc825c6e019a9f12cda7f1
Lunox-Code <lunox.code@gmail.com>
2020-10-27 21:45:45 -0600

    init

--- App.js
+++ App.js
@@ -1,40 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Button } from 'react-native';
 import { Header, Input, CheckBox, Image, ListItem, Avatar, Badge } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 
 export default function App() {
+  
   return (
-    <View>
+  // All inside this View
+  <View>
     
     <Header
-    // First topic HEADER
+      // Topic 1: HEADER
       leftComponent={{ icon: 'menu', color: '#fff' }}
       centerComponent={{ text: '100 Days Code - React Native Hear', style: { color: '#fff' } }}/>
     
+    <Text 
+      // Topic 2: Text Style
+      style={styles.headline}>Hi, Lunox!
+    </Text>
+
     <Input
-    // Second topic INPUT
+      // Topic 3: INPUT
       placeholder='INPUT: Write something here!'/>
+
+    <Button
+      // Topic 4: Button
+      title = 'Then, Click here'></Button>
     
     <CheckBox
-    // Third topic CheckBox
+      // Topic 5: CheckBox
       center
       title='Check Box: Click Here'/>
 
-<ListItem>
-  <Badge
-    value={10}
-    textStyle={{ color: 'orange' }}
-    containerStyle={{ marginTop: -20 }}
-  />
-</ListItem>
-    </View>
+    <ListItem>
+      <Badge
+        // Topic 6: Badge
+        value={10}
+        textStyle={{ color: 'white' }}
+        containerStyle={{ marginTop: -20 }}
+      ></Badge>
+    </ListItem>
+    
+  </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -42,4 +54,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  // For Topic 2: Text Style
+  headline: {
+    color: 'white', // <-- The magic
+    textAlign: 'center', // <-- The magic
+    fontWeight: 'bold',
+    fontSize: 50,
+    backgroundColor: 'purple',
+  }
 });
