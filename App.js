5340aa579f0a5b0a10a68844038bbcac653fbfde
Lunox-Code <lunox.code@gmail.com>
2020-10-27 20:19:36 -0600

    init

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Header, Input, CheckBox } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+    
+    <Header
+    // First topic HEADER
+      leftComponent={{ icon: 'menu', color: '#fff' }}
+      centerComponent={{ text: '100 Days Code - React Native Hear', style: { color: '#fff' } }}/>
+    
+    <Input
+    // Second topic INPUT
+      placeholder='INPUT: Write something here!'/>
+    
+    <CheckBox
+    // Third topic CheckBox
+      center
+      title='Check Box: Click Here'/>
+      
     </View>
   );
 }
